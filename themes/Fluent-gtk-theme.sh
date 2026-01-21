849132e745c292c6961d8e46fc262567fabead70
satella <kiyotakaa591@gmail.com>
2026-01-21 16:34:51 +0300

    Modify Fluent GTK theme installation script
    Updated installation script to include libsass1 and sassc packages.

--- themes/Fluent-gtk-theme.sh
+++ themes/Fluent-gtk-theme.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-# Fluent GTK tema kurulumu (XFCE, saydamlık yok) - Sisteme kurulum (/usr/share/themes)
 
-# Gereken paketleri yükle
 sudo apt update
-sudo apt install -y git
+sudo apt install -y git libsass1 sassc
 
-# Repo'yu klonla
 git clone https://github.com/vinceliuice/Fluent-gtk-theme.git "$HOME/satellaos-installer/themes/Fluent-gtk-theme/"
 
-# Klasöre geç
 cd $HOME/satellaos-installer/themes/Fluent-gtk-theme/ || exit
 
-# GTK temasını /usr/share/themes içine yükle (tüm temalar, saydamlık yok)
 sudo ./install.sh --dest /usr/share/themes --theme all --tweaks solid
