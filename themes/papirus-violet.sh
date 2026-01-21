114627e7573cdb6bde5e2390318f0352ebaa9d27
satella <kiyotakaa591@gmail.com>
2026-01-21 16:36:55 +0300

    Refresh icon cache in papirus-violet.sh
    Updated the script to ensure the icon cache is refreshed at the end.

--- themes/papirus-violet.sh
+++ themes/papirus-violet.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
-# Papirus-folders otomatik kurulum scripti
-# Debian 13 + XFCE için hazırlanmıştır
-
-echo ">>> Paket listesi güncelleniyor..."
 sudo apt update
 
-echo ">>> wget paketi kuruluyor (gerekliyse)..."
 sudo apt install -y wget
 
-echo ">>> Papirus ikon teması kuruluyor..."
 sudo apt install -y papirus-icon-theme
 
-echo ">>> papirus-folders kuruluyor..."
 wget -qO- https://git.io/papirus-folders-install | sh
 
-echo ">>> violet renk uygulanıyor..."
 papirus-folders -C violet --theme Papirus
 
-echo ">>> ikon önbelleği yenileniyor..."
 sudo gtk-update-icon-cache -f /usr/share/icons/*
-
-echo ">>> İşlem tamam! Oturumu kapatıp açarsanız değişiklikler görünür."
